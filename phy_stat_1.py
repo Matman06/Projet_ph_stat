4ec641386c4581a996d8ba11c9ccf2c5650ab2fb
Matman06 <63415111+Matman06@users.noreply.github.com>
2023-02-13 15:16:40 +0100

    Update phy_stat_1.py

--- phy_stat_1.py
+++ phy_stat_1.py
@@ -17,7 +17,20 @@ def partie_positive(a):
 
 def norme_VT():
 
+    def psi_1(k):
+    return b*v1 + math.log(n1-k)
+    
+def psi_2(k):
+    return b*v2 + math.log(n2-k)
+    
 ##L'algorithme
+
+def matrice_transition():
+    X = [[3,0],[2,1],[1,2],[0,3]]
+    M = np.zeros(k+1,k+1)
+    for s in range(k+1):
+        i = bern
+            
 
 def metropolis(beta,epsilon):
     L = [(100,100)] #on part initialement avec 100 particules sur le 1er état, et 0 sur le 2nd
